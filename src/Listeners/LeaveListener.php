f9fb3d6bc6b2079f4485b18ee3ddce55c5c40aa3
wuwx <wuweixin@gmail.com>
2018-11-21 22:07:24 +0800

    Listener

---
+++ src/Listeners/LeaveListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wuwx\LaravelWorkflow\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class LeaveListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+
+    }
+}
--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -3,13 +3,42 @@
 namespace Wuwx\LaravelWorkflow\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Wuwx\LaravelWorkflow\Events\GuardEvent;
+use Wuwx\LaravelWorkflow\Listeners\AnnounceListener;
+use Wuwx\LaravelWorkflow\Listeners\CompletedListener;
+use Wuwx\LaravelWorkflow\Listeners\EnteredListener;
+use Wuwx\LaravelWorkflow\Listeners\EnterListener;
+use Wuwx\LaravelWorkflow\Listeners\GuardListener;
+use Wuwx\LaravelWorkflow\Listeners\LeaveListener;
+use Wuwx\LaravelWorkflow\Listeners\TransitionListener;
 
 class EventServiceProvider extends ServiceProvider
 {
+
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        "workflow.guard"      => [GuardListener::class],
+        'workflow.leave'      => [LeaveListener::class],
+        'workflow.transition' => [TransitionListener::class],
+        'workflow.enter'      => [EnterListener::class],
+        'workflow.entered'    => [EnteredListener::class],
+        'workflow.completed'  => [CompletedListener::class],
+        'workflow.announce'   => [AnnounceListener::class],
+    ];
+
     protected $subscribe = [
 
     ];
 
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
     public function boot()
     {
         parent::boot();
